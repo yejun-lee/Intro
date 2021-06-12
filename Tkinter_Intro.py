67bffa6d10d4a8d2fa433f8ac95cf53ee6b3d525
Yejun Lee <85493705+yejun-lee@users.noreply.github.com>
2021-06-12 23:03:09 +0900

    Tkinter 함수 정리

--- Tkinter_Intro.py
+++ Tkinter_Intro.py
@@ -1,13 +1,17 @@
 from tkinter import *
+import tkinter.scrolledtext
 import pandas as pd
+from PIL import ImageTk
 import os   # 폴더를 만들고 삭제, 현재 폴더 위치
 
-# global dat
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
 
 print(os.getcwd())  # 현재 디렉토리 이름 표시
 
 ## 파일 불러오기
-dat = pd.read_csv("./sejong.csv", sep='\t')
+dat = pd.read_csv("./의료용어.csv")
+subset = dat.head(n=621)[['용어']]
 
 ## 기능 추가
 ## 제출 버튼을 클릭했을 때, 동작 기능.
@@ -18,39 +22,56 @@ def click():
     output.delete(0.0, END)   # 텍스트 박스 내용을 지운다.
 
     try:
-        def_word = dat.loc[dat['word']==word, 'def'].values[0]  # values = 0인 이유 : pandas
+        def_word = dat.loc[dat['용어']==word, '정의'].values[0]  # values = 0인 이유 : pandas
         # word 안의 단어를 찾고 그 단어의 def 값을 반환함
     except:
         def_word = '단어의 뜻을 찾을 수 없음'
 
     output.insert(END, def_word)
 
-window = Tk()  # 창을 하나 만든다.
-window.title('My Dictionary')
+window = Tk()
+window.title('의료시험용어사전')
 
 # 01 입력 상자 설명 레이블
 label = Label(window, text='원하는 단어 입력 후, 엔터 키 누르기')
-label.grid(row=0, column=0, sticky=W)   # sticky : 어디에다가 위치시킬 것인가. W = west
+label.grid(row=0, column=0, sticky=W, pady=10, padx=5)
 
 # 02 텍스트 입력이 가능한 상자(Entry)
-entry = Entry(window, width=15, bg='light green')
-entry.grid(row=1, column=0, sticky=W)
+entry = Entry(window, width=15, bg='light blue')
+entry.grid(row=1, column=0, sticky=W, pady=10, padx=10)
 
 # 03 제출 버튼 만들기
 button = Button(window, text='제출', width=5, command=click)   # command에 함수 전달, 클릭했을 때 함수 실행
-button.grid(row=2, column=0, sticky=W)
+button.grid(row=2, column=0, sticky=W, pady=10, padx=10)
 
 # 04 설명 레이블 - 의미
 label_1 = Label(window, text='\n의미')
-label_1.grid(row=3, column=0, sticky=W)
+label_1.grid(row=3, column=0, sticky=W, pady=10, padx=5)
 
 # 05 텍스트 박스 입력 상자
 # columnspan=2 는 (4,0) ~ (4,1) 위치까지 분포
-output = Text(window, width=50, height=6, wrap=WORD, background='light green')
-output.grid(row=4, column=0, columnspan=2, sticky=W)
+output = Text(window, width=50, height=6, wrap=WORD, background='light blue')
+output.grid(row=4, column=0, columnspan=2, sticky=W, pady=10, padx=10)
+
+# 06 용어를 출력하기 위한 스크롤 바 생성
+label_2 = Label(window, text='\n용어 목록')
+label_2.grid(row=5, column=0, sticky=W, pady=10, padx=5)
+
+scrollbar = tkinter.scrolledtext.ScrolledText(window, background='light blue')
+scrollbar.grid(row=6, column=0, sticky=W, pady=10, padx=10)
+scrollbar.insert(INSERT, subset)
+scrollbar.configure(state='disabled')
+
+# 07 이미지 삽입
+Image_1 = ImageTk.PhotoImage(file='dog_e.png')
+label_3 = Label(window, background='white', relief='sunken', image=Image_1)
+label_3.place(x=365, y=15, width=230, height=200)
 
 print(dat.shape)
 print(dat.columns)
+print(dat.head())
 
-# 메인 반복문 실행
 window.mainloop()
+
+## 목록 출력기능 만들어보기
+## 외관 깔끔하게 만들기
